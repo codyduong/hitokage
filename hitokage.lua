77574831a701ff931d9ca30198b27dc7e9cef0a4
Cody Duong <duongcody507@gmail.com>
2024-06-23 13:19:34 -0500

    Add lua types

---
+++ hitokage.lua
@@ -0,0 +1,130 @@
+--- @meta hitokage
+
+---
+---
+---
+--- [View documents]()
+---
+--- @class hitokagelib
+_G.hitokage = {}
+_G._not_deadlocked = function() end
+
+-------------------------------------------------------------------------------
+--- generic global functions
+
+--- @vararg any
+function hitokage.debug(...) end
+--- @vararg any
+function hitokage.error(...) end
+--- @vararg any
+function hitokage.info(...) end
+
+--- Sleep function in milliseconds
+--- @param ms number Amount of time to sleep.
+function hitokage.sleep_ms(ms) end
+
+-------------------------------------------------------------------------------
+--- Represents the bar module
+--- @class bar
+local bar = {}
+
+--- Links to WorkspaceProps in 'hitokage-core\src\widgets\workspace.rs'
+--- @class WorkspaceProps
+
+--- Links to ClockProps in 'hitokage-core\src\widgets\clock.rs'
+--- @class ClockProps
+--- @fields format string?
+
+--- Links to BoxProps in 'hitokage-core\src\widgets\box.rs'
+--- @class BoxProps
+
+--- Links to WidgetProps in 'hitokage-core\src\widgets\mod.rs'
+--- @class WidgetProps
+--- @field Workspace table? Optional workspace configuration
+--- @field Clock ClockProps? Optional clock configuration
+--- @field Box table? Optional box configuration
+
+--- Array of WidgetProps
+--- @alias WidgetPropsVec table<number, WidgetProps>
+
+--- @class BarProps
+--- @field widgets WidgetPropsVec
+
+--- Links to BarInstanceUserData in 'hitokage-lua\src\widgets\bar.rs'
+--- @class BarInstance
+--- Wrapper around bar:is_ready()
+--- @field ready boolean
+--- Wrapper around bar:get_widgets()
+--- @field widgets boolean -- @codyduong TODO fix this return type
+--- Wrapper around bar:get_geometry()
+--- @field geometry boolean
+local bar_instance = {}
+
+--- @alias BarInstanceArray table<number, BarInstance>
+
+--- Gets the bar id
+--- @return number
+function bar_instance:get_id() end
+
+--- Checks if the bar has been instantiated in gtk4
+--- @return boolean
+function bar_instance:is_ready() end
+
+--- Gets the widgets on the bar
+--- @return boolean -- @codyduong TODO fix this return type
+function bar_instance:get_widgets() end
+
+--- Gets the geometry of the bar
+--- @return MonitorGeometry
+function bar_instance:get_geometry() end
+
+--- Creates a new bar
+--- @param args BarProps
+--- @return BarInstance
+function bar.create(args) end
+
+-------------------------------------------------------------------------------
+--- Represents the monitor module
+--- @class monitor
+local monitor = {}
+
+--- Links to 'pub struct MonitorGeometry' in 'hitokage-core\src\lua\monitor.rs'
+--- @class MonitorGeometry
+--- @field x number
+--- @field y number
+--- @field width number
+--- @field height number
+
+--- Links to 'pub struct Monitor' in 'hitokage-core\src\lua\monitor.rs'
+--- @class Monitor
+--- @field connecter string | nil,
+--- @field description string | nil,
+--- @field geometry MonitorGeometry,
+--- @field manufacturer string | nil,
+--- @field model string | nil,
+--- This value is in millihertz (mHz) not hertz (Hz)
+--- @field refresh_rate number,
+--- @field is_primary boolean,
+--- @field device string,
+--- @field device_id string,
+--- @field id number,
+--- @field name string,
+--- @field scale_factor MonitorScaleFactor,
+
+--- @alias MonitorVec table<number, Monitor>
+
+--- Links to 'pub struct MonitorScaleFactor' in 'hitokage-core\src\lua\monitor.rs'
+--- @class MonitorScaleFactor
+--- @field x number,
+--- @field y number,
+
+--- @return MonitorVec
+function monitor.get_all() end
+
+--- @return Monitor
+function monitor.get_primary() end
+
+-------------------------------------------------------------------------------
+--- Compose all the modules
+hitokage.bar = bar
+hitokage.monitor = monitor
