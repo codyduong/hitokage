4cb8a4ed4cf94c93a7dd52f23813dd2e650e2e08
Cody Duong <duongcody507@gmail.com>
2024-07-17 13:43:40 -0500

    rename workspace props, add lua support

--- hitokage-lua/src/widgets/workspace.rs
+++ hitokage-lua/src/widgets/workspace.rs
@@ -1,25 +1,50 @@
+use crate::{impl_getter_fn, impl_setter_fn};
+use hitokage_core::widgets::workspace::WorkspaceMsg;
+use hitokage_core::widgets::workspace::WorkspaceMsgHook::{GetItemHeight, GetItemWidth, SetItemHeight, SetItemWidth};
+use mlua::Lua;
 use mlua::{LuaSerdeExt, UserData, UserDataMethods, Value};
+use std::sync::mpsc;
 
 #[derive(Debug, Clone)]
 pub struct WorkspaceUserData {
   pub r#type: String,
+  pub sender: relm4::Sender<WorkspaceMsg>,
+}
+
+impl WorkspaceUserData {
+  fn sender(&self) -> Result<relm4::Sender<WorkspaceMsg>, crate::HitokageError> {
+    Ok(self.sender.clone())
+  }
+
+  impl_getter_fn!(get_item_height, WorkspaceMsg::LuaHook, GetItemHeight, u32);
+  impl_setter_fn!(set_item_height, WorkspaceMsg::LuaHook, SetItemHeight, u32);
+
+  impl_getter_fn!(get_item_width, WorkspaceMsg::LuaHook, GetItemWidth, u32);
+  impl_setter_fn!(set_item_width, WorkspaceMsg::LuaHook, SetItemWidth, u32);
 }
 
 impl UserData for WorkspaceUserData {
   fn add_methods<'lua, M: UserDataMethods<'lua, Self>>(methods: &mut M) {
     methods.add_method("get_type", |_, this, _: ()| Ok(this.r#type.clone()));
 
-    methods.add_meta_method(
+    methods.add_method("get_item_height", |_, this, _: ()| Ok(this.get_item_height()?));
+    methods.add_method("set_item_height", |lua, this, value: mlua::Value| {
+      Ok(this.set_item_height(lua, value)?)
+    });
+
+    methods.add_method("get_item_width", |_, this, _: ()| Ok(this.get_item_width()?));
+    methods.add_method("set_item_width", |lua, this, value: mlua::Value| {
+      Ok(this.set_item_width(lua, value)?)
+    });
+
+    methods.add_meta_method::<_, mlua::String, _>(
       "__index",
-      |lua, instance, value| -> Result<mlua::Value<'lua>, mlua::Error> {
-        match value {
-          Value::String(s) => match s.to_str()? {
-            "type" => Ok(lua.to_value(&instance.r#type.clone())?),
-            _ => Ok(Value::Nil),
-          },
+      |lua, instance, key| -> Result<mlua::Value<'lua>, mlua::Error> {
+        match key.to_str().unwrap() {
+          "type" => Ok(lua.to_value(&instance.r#type.clone())?),
           _ => Ok(Value::Nil),
         }
       },
-    )
+    );
   }
 }
