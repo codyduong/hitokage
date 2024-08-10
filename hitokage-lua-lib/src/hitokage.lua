164b51289d8ac6e6b4ffdafc985a84749ecaf983
Cody Duong <duongcody507@gmail.com>
2024-08-10 15:46:11 -0500

    doc: update hitokage_lua_lib libs

--- hitokage-lua-lib/src/hitokage.lua
+++ hitokage-lua-lib/src/hitokage.lua
@@ -62,10 +62,23 @@ function hitokage.timeout(timeout, action) end
 
 -------------------------------------------------------------------------------
 --- Compose hitokage
+
+--- Represents the experimental module. Code that lives here is subject to
+--- breaking API changes, typically for allowing users to access latest
+--- features that may still be undergoing design.
+--- 
+--- @class Experimental
+local experimental = {}
+
+--- Represents the unsafe module. Code that bypasses rust safety, and therefore
+--- with improper usage is possible to encounter a variety of errors.
+--- 
+--- @class Unsafe
 local unsafe = {
   reactive = reactive
 }
 
 hitokage.bar = bar
+hitokage.experimental = experimental
 hitokage.monitor = monitor
 hitokage.unsafe = unsafe
